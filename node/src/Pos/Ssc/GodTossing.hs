2875ed53aa438c098b49c0f06d6094185092856a
James Michael DuPont <jamesmikedupont+build@gmail.com>
2017-12-30 05:43:24 -0500

    bugfix in docu, wrong url

--- node/src/Pos/Ssc/GodTossing.hs
+++ node/src/Pos/Ssc/GodTossing.hs
@@ -4,7 +4,7 @@
 -- exchange commitments, openings, and shares, and in the end arrive
 -- at a shared seed.
 --
--- See https://eprint.iacr.org/2015/889.pdf (“A Provably Secure
+-- See https://eprint.iacr.org/2016/889.pdf (“A Provably Secure
 -- Proof-of-Stake Blockchain Protocol”), section 4 for more details.
 
 module Pos.Ssc.GodTossing
